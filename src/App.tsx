0e5d01a70fbe1b3a8040c523456ec5b73c39273f
velrino <denismagalhaesvelrino@gmail.com>
2023-05-28 16:19:46 -0300

    Dark and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-
+import { useState } from "react";
+import { ConfigProvider, theme } from 'antd';
 import { MainRoutes } from "./Routes";
 
 export default function App() {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
   return (<>
-    <MainRoutes />
+    <ConfigProvider theme={{
+      algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+    }}>
+      <div className={isDarkMode ? "base-content dark-theme" : "base-content light-theme"}>
+        <MainRoutes />
+      </div>
+    </ConfigProvider>
   </>
   );
 }
